1b6fa613f4d0fbc64b202ce399c94a616f6f3768
sameena <98093686+Sameena7676@users.noreply.github.com>
2022-02-01 18:12:14 +0530

    Add files via upload

---
+++ session4/UncheckedException2.java
@@ -0,0 +1,15 @@
+package excption;
+
+public class UncheckedException2 {
+
+	public static void main(String[] args) {
+		
+		String name=null;
+		
+		if(name.equals("admin"))
+			System.out.println("Valid user");
+		else
+			System.out.println("Invalid user");
+	}
+
+}
---
+++ session4/UncheckedException3.java
@@ -0,0 +1,12 @@
+package excption;
+
+public class UncheckedException3 {
+
+
+	public static void main(String[] args) {
+		
+		int array[]= {12,3,4,6,7};
+		
+		System.out.println(array[5]);
+	}
+}
