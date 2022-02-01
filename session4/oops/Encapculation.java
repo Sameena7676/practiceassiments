73dd68fa6986ffe396dd95e47162691bf8ac26de
sameena <98093686+Sameena7676@users.noreply.github.com>
2022-02-01 18:40:14 +0530

    Add files via upload

---
+++ session4/oops/Encapculation.java
@@ -0,0 +1,33 @@
+package oops;
+
+public class Encapculation {
+
+
+	private int id;
+	private String name;
+	private String email;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	//get always return a value of same type of that variable
+	//set method always a void type with one parameter
+	
+
+}
