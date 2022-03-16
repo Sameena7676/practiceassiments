96880b438dc683f5b02946e10b6f24ed6e705049
sameena <98093686+Sameena7676@users.noreply.github.com>
2022-03-16 13:40:15 +0530

    Add files via upload

---
+++ phase-3/DisplayUserFeedback/src/test/java/com/project/Feedback/DisplayUserFeedbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Feedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisplayUserFeedbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
